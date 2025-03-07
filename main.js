bc08441fcf2aa1a87b8ee27aa1a069b218049f88
conny-coder <kostia.shkambula@gmail.com>
2025-03-07 11:49:28 +0200

    new version 2

--- main.js
+++ main.js
@@ -5,17 +5,24 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Particle {
-  constructor(x, y, image, spline) {
+  constructor(x, y, delay, speed, image, spline) {
     this.x = x;
     this.y = y;
     this.image = image;
     this.t = 0;
     this.spline = spline;
+    this.delay = delay;
+    this.speed = speed;
   }
 
-  update(precomputed, speed) {
+  update(precomputed) {
+    if (this.delay > 0) {
+      this.delay -= 0.01;
+      return;
+    }
+
     if (this.t < 1) {
-      this.t += speed;
+      this.t += this.speed;
       const pos = this.spline(precomputed, this.t);
       this.x = pos.x;
       this.y = pos.y;
@@ -32,18 +39,16 @@ class ParticleManager {
     this.particles = [];
   }
 
-  addParticle(x, y, points, speed, spline) {
+  addParticle(x, y, delay, points, src, speed, spline) {
     const img = new Image();
-    img.src = "./star.svg";
-    const particle = new Particle(x, y, img, spline);
-    particle.points = points;
-    particle.speed = speed;
+    img.src = src;
+    const particle = new Particle(x, y, delay, speed, img, spline);
     particle.precomputed = catmullRomPrecompute(points);
     this.particles.push(particle);
   }
 
   update() {
-    this.particles.forEach((p) => p.update(p.precomputed, p.speed));
+    this.particles.forEach((p) => p.update(p.precomputed));
   }
 
   draw(ctx) {
@@ -51,19 +56,16 @@ class ParticleManager {
   }
 }
 
-// Предварительный расчёт контрольных точек для Catmull-Rom
 function catmullRomPrecompute(points, alpha = 0.5) {
   const p0 = points[0];
   const p1 = points[1];
   const p2 = points[2];
   const p3 = points.length > 3 ? points[3] : p2;
 
-  // Расстояния между точками
   const d1 = Math.hypot(p1.x - p0.x, p1.y - p0.y);
   const d2 = Math.hypot(p2.x - p1.x, p2.y - p1.y);
   const d3 = Math.hypot(p3.x - p2.x, p3.y - p2.y);
 
-  // Вычисляем степени расстояний
   const d1a = Math.pow(d1, alpha);
   const d2a = Math.pow(d2, alpha);
   const d3a = Math.pow(d3, alpha);
@@ -71,7 +73,6 @@ function catmullRomPrecompute(points, alpha = 0.5) {
   const d2a2 = Math.pow(d2, 2 * alpha);
   const d3a2 = Math.pow(d3, 2 * alpha);
 
-  // Вычисляем коэффициенты для преобразования в кубическую Bézier
   const A = 2 * d1a2 + 3 * d1a * d2a + d2a2;
   const B = 2 * d3a2 + 3 * d3a * d2a + d2a2;
   let N = 3 * d1a * (d1a + d2a);
@@ -117,12 +118,14 @@ const particleManager = new ParticleManager();
 particleManager.addParticle(
   100,
   300,
+  4,
   [
     { x: 100, y: 400 },
     { x: 100, y: 300 },
     { x: 900, y: 200 },
     { x: 700, y: 300 },
   ],
+  "./star.svg",
   0.005,
   catmullRomAtT
 );
@@ -131,12 +134,14 @@ particleManager.addParticle(
 particleManager.addParticle(
   150,
   350,
+  3,
   [
     { x: 150, y: 450 },
     { x: 150, y: 350 },
     { x: 850, y: 250 },
     { x: 650, y: 350 },
   ],
+  "./bell.svg",
   0.006,
   catmullRomAtT
 );
@@ -145,12 +150,14 @@ particleManager.addParticle(
 particleManager.addParticle(
   200,
   400,
+  2,
   [
     { x: 200, y: 500 },
     { x: 200, y: 400 },
     { x: 800, y: 300 },
     { x: 600, y: 400 },
   ],
+  "./star.svg",
   0.007,
   catmullRomAtT
 );
@@ -159,12 +166,14 @@ particleManager.addParticle(
 particleManager.addParticle(
   250,
   450,
+  1,
   [
     { x: 250, y: 550 },
     { x: 250, y: 450 },
     { x: 750, y: 350 },
     { x: 550, y: 450 },
   ],
+  "./bell.svg",
   0.005,
   catmullRomAtT
 );
